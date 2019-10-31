Correct normalization of images

python-connector-api/OurCodes/OpticalFlow/OptFlow.py
@@ -21,7 +21,7 @@ def interpolate_frames(frame, coords, flow, n_frames):
 
 def OpticalFlow(Gold, Gnew, nframes):
 
-    t0 = time.time()
+    t1 = time.time()
 
     gridoldF=np.float64( Gold ).T
     gridnewF=np.float64( Gnew ).T
@@ -38,23 +38,20 @@ def OpticalFlow(Gold, Gnew, nframes):
 
     aux = 1.0 / (maxAll-minAll) 
 
-    gridoldF = (gridoldF-minAll)*aux*256
-    gridnewF = (gridnewF-minAll)*aux*256
-
-    t1 = time.time()
-
-
+    gridoldF = (gridoldF-minAll)*aux* 65535 
+    gridnewF = (gridnewF-minAll)*aux* 65535 
 
     t2 = time.time()
 
-    optflow_params = [0.5, 7, 4*15, 3, 7, 1.5, 0]
+
+    optflow_params = [0.5, 6 , 40 , 3 , 5, 1.2, 0]
     flowUp   = cv2.calcOpticalFlowFarneback(gridnewF, gridoldF, None, *optflow_params)
     flowDown = cv2.calcOpticalFlowFarneback(gridoldF, gridnewF, None, *optflow_params)
     
-    t3 = time.time()
 
-    
-    
+   
+    t3 = time.time()
+ 
 
     lx = gridold.shape[0]
     ly = gridold.shape[1]
@@ -73,18 +70,18 @@ def OpticalFlow(Gold, Gnew, nframes):
 
     for i in range(N):
         alpha = float(i)/float(N)
-        inter_frames.append(inter_framesUp[i]*(1.0-alpha) + inter_framesDown[-i]*alpha)
+        inter_frames.append( inter_framesUp[i]*(1.0-alpha) + inter_framesDown[-i]*alpha) 
     
     inter_frames.append(gridnewF)
     
     t5 = time.time()
 
     print("Running for ", nframes, "frames.")
-    print("Normalization time=",t1-t0)
+    print("Normalization time=",t2-t1)
     print("Optical Flow time =",t3-t2)
     print("Interpolation time=",t4-t3)
     print("final loop time   =",t5-t4)
-    print("Total time        =",t5-t0)
+    print("Total time        =",t5-t1)
 
     return inter_frames
 
@@ -109,10 +106,12 @@ parameter_grid = 'wind_speed_10m:kmh'
 # LON: 64.4
 
 #Nice weather close to France
-#lat_N = 45
-#lon_W = -25
-#lat_S = 35
-#lon_E = 5
+lat_N = 45
+lon_W = -25
+lat_S = 35
+lon_E = 5
+
+
 
 #Hurricane
 lat_N = 25
@@ -121,8 +120,8 @@ lat_S = 15
 lon_E = 70
 
 
-res_lat = 0.02
-res_lon = 0.02
+res_lat = (lat_N - lat_S)/256.  
+res_lon = (lon_E - lon_W)/256.  
 
 
 try:
@@ -141,14 +140,11 @@ except Exception as e:
 gridnew = gridnew.to_numpy()
 gridold = gridold.to_numpy()
 
-
-
+#gridnew = np.load("gridnew.npy")
+#gridold = np.load("gridold.npy")
 
 
 inter_frames = OpticalFlow(gridold, gridnew, 10)
-
-
-
 
 
 fig = plt.figure(dpi=100)